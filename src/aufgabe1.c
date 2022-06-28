ce4dad171b5438cbf0ecc62dfc569d4ad4b19805
bayerju <bayerju@users.noreply.github.com>
2022-06-28 22:20:17 +0200

    aufgabe1 und clang

--- src/aufgabe1.c
+++ src/aufgabe1.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("hello world");
+double geoReihe(int n) {
+    int i = 0;
+    double sum = 1, q=0.5, ai = 1;
+    while(i<n) {
+        ai = q * ai;
+        sum += ai;
+        i++;
+    }
+    return sum;
+}
+
+int main() {
+    double result = geoReihe(4);
+
+    printf("Ergebnis: %f", result);
     return 0;
 }
